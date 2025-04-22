43d8ea07db460b1b1aa4fdf2edb8cb1afd4e2f45
Mohammed Lafrikh <mohamedlafrikh26@gmail.com>
2025-04-22 23:02:08 +0100

    Initial commit - CodeIgniter test project

---
+++ app/Config/Database.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Database\Config;
+
+/**
+ * Database Configuration
+ */
+class Database extends Config
+{
+    /**
+     * The directory that holds the Migrations
+     * and Seeds directories.
+     */
+    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+
+    /**
+     * Lets you choose which connection group to
+     * use if no other is specified.
+     */
+    public string $defaultGroup = 'default';
+
+    /**
+     * The default database connection.
+     *
+     * @var array<string, mixed>
+     */
+    public array $default = [
+        'DSN'          => '',
+        'hostname'     => 'localhost',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8',
+        'DBCollat'     => 'utf8_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+    ];
+
+    /**
+     * This database connection is used when
+     * running PHPUnit database tests.
+     *
+     * @var array<string, mixed>
+     */
+    public $tests = [
+        'DSN'      => '',
+        'hostname' => 'localhost',  // Adresse de votre serveur MySQL
+        'username' => 'root',       // Votre nom d'utilisateur MySQL
+        'password' => '',           // Votre mot de passe MySQL (ou laissez vide si aucun)
+        'database' => 'test_db',    // Nom de la base de données MySQL pour les tests
+        'DBDriver' => 'MySQLi',     // Utilisation de MySQLi pour la connexion
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => (ENVIRONMENT !== 'production'),
+        'cacheOn'  => false,
+        'cacheDir' => '',
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+    ];
+    
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Ensure that we always set the database group to 'tests' if
+        // we are currently running an automated test suite, so that
+        // we don't overwrite live data on accident.
+        if (ENVIRONMENT === 'testing') {
+            $this->defaultGroup = 'tests';
+        }
+    }
+}
